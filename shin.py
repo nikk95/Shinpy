055dcb7f1bafb07d85cb67e01dc0f469d5b3fa47
Gaurav Koley <arkokoley@live.in>
2014-07-30 19:37:59 +0530

    shinpy appears

--- shin.py
+++ shin.py
@@ -2,3 +2,12 @@
 print "My Name Is Shinpy"
 print "Just Getting Started"
 print "I Love Shaelyn But She Hates Me" #True Story
+print "/^-----^\ "
+print "V  o o  V "
+print " |  Y  | "
+print "  \ Q /  "
+print "  / - \  "
+print "  |    \ "
+print "  |     \    ) "
+print "  || (___\==== "
+
